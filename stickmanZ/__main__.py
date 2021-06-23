4e8df5d3b351598d3fcf53bd9338208ee26567d7
MichaelMcFarland98 <michaelmcfarland1998@gmail.com>
2021-06-23 10:04:30 -0600

    6/23 changes

--- stickmanZ/__main__.py
+++ stickmanZ/__main__.py
@@ -1,1 +1,16 @@
-# program entry point
+import arcade
+from stickmanZ.game.display_screen import display_screen 
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Stickman Z"
+
+def main():
+    game = display_screen(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+    
+
+if __name__ == "__main__":
+    main()
+
